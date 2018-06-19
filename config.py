934db0fa18bfc45ef46d0db68b69ab55d8230eb6
Marcio Vinicius <marcio.campos@outlook.com>
2018-06-19 20:58:51 -0300

    Alteração da conexão do banco de dados

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
-MONGO_URI='mongodb://ada:chuvinha10@ds043358.mlab.com:43358/ubiqs'
+#MONGO_URI='mongodb://ada:chuvinha10@ds043358.mlab.com:43358/ubiqs'
+
+MONGO_URI='mongodb://marcio:marcio@ds213338.mlab.com:13338/marcio_db'
+
